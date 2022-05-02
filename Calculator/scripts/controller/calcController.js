89c46b494678874705e5a13524695866e9258421
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-05-02 19:45:38 -0300

    Update calcController.js

--- Calculator/scripts/controller/calcController.js
+++ Calculator/scripts/controller/calcController.js
@@ -12,6 +12,31 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initiButtonsEvents();
+        this.initKeyBoard();
+        
+    }
+
+    pasteFromClipBoard(){
+
+        document.addEventListener('paste', e=>{
+
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        });
+    }
+
+    copyToClipBoard(){
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
     }
 
     initialize(){
@@ -24,6 +49,58 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipBoard();
+    }
+
+    initKeyBoard(){
+
+        document.addEventListener('keyup', e=>{
+            
+            switch (e.key){
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+    
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+                
+                case '.':
+                case ',':
+                    this.addDot('.');
+                    break;
+                
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key))
+                    break;
+                
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipBoard();
+                    break;
+            }
+        });
     }
 
     addEventListenerAll(element, events, fn){
