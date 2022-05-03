ef2b5545b7290f57c06172eb153b029fc1233fef
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-05-03 19:00:10 -0300

    Update

--- Users_Management/index.js
+++ Users_Management/index.js
@@ -1,25 +1,16 @@
-var name = document.querySelector("#exampleInputName");
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var photo = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
-
-
-
 var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 fields.forEach((field, index)=>{
     
     if(field.name == "gender" && field.checked){
 
-        console.log("SIM", field)
+        user[field.name] = field.value;
     }
     else {
-        console.log("NAO")
+        user[field.name] = field.value;
     }
 
-    //console.log(field.name);
 });
+
+console.log(user);
