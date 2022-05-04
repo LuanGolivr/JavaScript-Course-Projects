f8a607ec36e2c8bf6300d58c4e4cedd67bc2458b
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-05-03 23:46:12 -0300

    Update UserControllers.js

--- Users_Management/controllers/UserControllers.js
+++ Users_Management/controllers/UserControllers.js
@@ -16,36 +16,51 @@ class UserControllers {
 
             let values = this.getValues();
             
-
-            this.getPhoto((content)=>{
+            this.getPhoto().then((content)=>{
 
                 values.photo = content;
                 this.addLine(values);
+            }, (e)=>{
+                console.error(e);
             });
 
-            
         });
     }
 
-    getPhoto(callback){
+    getPhoto(){
 
-        let fileReader = new FileReader();
+        return new Promise((resolve, reject)=>{
 
-        let elements = [...this.formEl.elements].filter(item =>{
+            let fileReader = new FileReader();
 
-            if (item.name === 'photo'){
-                return item;
+            let elements = [...this.formEl.elements].filter(item =>{
+
+                if (item.name === 'photo'){
+                    return item;
+                }
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = (e)=>{
+
+                reject(e);
+            };
+
+            if(file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
             }
+            
         });
 
-        let file = elements[0].files[0];
-
-        fileReader.onload = ()=>{
-
-            callback(fileReader.result);
-        };
-
-        fileReader.readAsDataURL(file);
+        
     }
 
     getValues(){
@@ -58,7 +73,10 @@ class UserControllers {
         
                 user[field.name] = field.value;
             }
-            else {
+            else if( field.name == "admin"){
+
+                user[field.name] = field.checked;
+            } else{
                 user[field.name] = field.value;
             }
         
@@ -78,18 +96,20 @@ class UserControllers {
 
     addLine(dataUser){
 
-        this.tableEl.innerHTML = `
-            <tr>
-                <td><img src=${dataUser.photo} alt="User Image" class="img-circle img-sm"></td>
-                <td>${dataUser.name}</td>
-                <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
-                <td>
-                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                </td>
-            </tr>
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
+            <td><img src=${dataUser.photo} alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${(dataUser.admin ? 'Sim' : 'Não')}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
         `;
+
+        this.tableEl.appendChild(tr);
     }
 }
