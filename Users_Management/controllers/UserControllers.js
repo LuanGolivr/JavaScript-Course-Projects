46e60687ce09aba7cc58e03945a5912ec2a4cf04
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-05-07 16:44:27 -0300

    Update

--- Users_Management/controllers/UserControllers.js
+++ Users_Management/controllers/UserControllers.js
@@ -72,8 +72,15 @@ class UserControllers {
     getValues(){
 
         let user = {};
+        let isValid = true;
         
         [...this.formEl.elements].forEach((field, index)=>{
+
+            if (['name', 'email', 'password'].indexOf(field.name) >= -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
     
             if(field.name == "gender" && field.checked){
         
@@ -87,7 +94,11 @@ class UserControllers {
             }
         
         });
-    
+        
+        if(!isValid){
+            return false;
+        }
+
         return  new User(
             user.name, 
             user.gender, 
