141f146c2f9c80409d91681f13bcd71024ae49dc
Luan Oliveira <luangoncalvesoliveira@gmail.com>
2022-05-09 08:33:08 -0300

    Update

--- Users_Management/controllers/UserControllers.js
+++ Users_Management/controllers/UserControllers.js
@@ -1,8 +1,9 @@
 class UserControllers {
 
-    constructor(formId, tableId){
+    constructor(formIdCreate, formIdUpdate, tableId){
 
-        this.formEl = document.getElementById(formId);
+        this.formEl = document.getElementById(formIdCreate);
+        this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
         
         this.onSubmit();
@@ -15,6 +16,76 @@ class UserControllers {
 
             this.showPanelCreate();
         });
+
+        this.formUpdateEl.addEventListener('submit', e=>{
+
+            e.preventDefault();
+            let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+            let values = this.getValues(this.formUpdateEl);
+
+            let index = this.formUpdateEl.dataset.trIndex;
+            let tr = this.tableEl.rows[index];
+            tr.dataset.user = JSON.stringify(values);
+            
+            tr.innerHTML = `
+                <td><img src=${values.photo} alt="User Image" class="img-circle img-sm"></td>
+                <td>${values.name}</td>
+                <td>${values.email}</td>
+                <td>${(values.admin ? 'Sim' : 'Não')}</td>
+                <td>${Utils.dateFormart(values.register)}</td>
+                <td>
+                <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                </td>
+            `;
+
+
+            this.addEventsTr(tr);
+            this.updateCount();
+        });
+
+    }
+
+    addEventsTr(tr){
+
+        tr.querySelector(".btn-edit").addEventListener("click", e=>{
+
+            let json = JSON.parse(tr.dataset.user);
+            let form = document.querySelector("#form-user-update");
+
+            form.dataset.trIndex = tr.sectionRowIndex;
+
+            for(let name in json){
+
+                let field = form.querySelector("[name ="+ name.replace("_", "") + "]");
+
+                if(field){
+                    switch(field.type){
+
+                        case 'file':
+                            continue;
+                            break;
+                        
+                        case 'radio':
+                            field = form.querySelector("[name ="+ name.replace("_", "") + "][value="+ json[name] +"]");
+                            field.checked = true;
+                            break;
+                        
+                        case 'checkbox':
+                            field.checked = json[name];
+                            break;
+                        
+                        default:
+                            field.value = json[name];
+                    }     
+                }
+                
+            }
+
+            this.showPanelUpdate();
+        });
     }
 
     onSubmit(){
@@ -26,7 +97,7 @@ class UserControllers {
             let btn = this.formEl.querySelector("[type=submit]");
 
             btn.disabled = true;
-            let values = this.getValues();
+            let values = this.getValues(this.formEl);
 
             if(!values){
                 return false;
@@ -84,12 +155,12 @@ class UserControllers {
         
     }
 
-    getValues(){
+    getValues(formEl){
 
         let user = {};
         let isValid = true;
         
-        [...this.formEl.elements].forEach((field, index)=>{
+        [...formEl.elements].forEach((field, index)=>{
 
             if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value){
                 field.parentElement.classList.add('has-error');
@@ -147,6 +218,8 @@ class UserControllers {
 
             let json = JSON.parse(tr.dataset.user);
             let form = document.querySelector("#form-user-update");
+
+            form.dataset.trIndex = tr.sectionRowIndex;
 
             for(let name in json){
 
